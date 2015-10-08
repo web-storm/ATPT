951e7468580d7d9f1647dd4aded61ed55e1a7c55
web-storm <s-t-o-r-m@list.ru>
2015-10-08 11:40:21 +0300

    first commit

---
+++ Tatp.Visual/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+using Tatp.Core;
+
+namespace Tatp.Visual
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var mas = ArrayService.ArrayCreate(5);
+        }
+    }
+}
